583eab0877cc970ae76b32c79056934b5aec4178
Michael Raynes <raynesmike@yahoo.com>
2020-01-31 23:09:10 -0600

    added authentication

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+/*middleware function will just have access to request adn response cycle
+    next is a callback/follow-up function that will call the next midwarefunction
+*/
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // Check if no token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
